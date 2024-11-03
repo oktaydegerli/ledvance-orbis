a770972f2a7c162ca95aee966ed1ed8cd930e279
oktaydegerli <oktaydegerli@gmail.com>
2024-11-03 19:54:46 +0300

    Update

--- custom_components/ledvance_orbis/light.py
+++ custom_components/ledvance_orbis/light.py
@@ -1,4 +1,4 @@
-from homeassistant.components.light import LightEntity
+from homeassistant.components.light import LightEntity, ATTR_BRIGHTNESS
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -26,6 +26,7 @@ class LedvanceOrbis(LightEntity):
         self._device_ip = device_ip
         self._local_key = local_key
         self._is_on = False
+        self._brightness = 255
         self._name = "Ledvance Orbis"
         self._unique_id = f"ledvance_orbis_{device_id}"
         self._device = None
@@ -57,14 +58,27 @@ class LedvanceOrbis(LightEntity):
     def is_on(self):
         """Return true if light is on."""
         return self._is_on
+    
+    @property
+    def brightness(self):
+        """Return the brightness of the light."""
+        return self._brightness
+
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        return SUPPORT_BRIGHTNESS
 
     async def async_turn_on(self, **kwargs):
         """Turn on the light."""
         def turn_on():
             try:
-                self._device.set_multiple_values({
-                    '20': True
-                })
+                values = {'20': True}
+                if ATTR_BRIGHTNESS in kwargs:
+                    brightness = kwargs[ATTR_BRIGHTNESS]
+                    tuya_brightness = int((brightness / 255) * 1000)
+                    values['22'] = tuya_brightness
+                self._device.set_multiple_values(values)
                 return True
             except Exception as e:
                 return False
@@ -72,6 +86,8 @@ class LedvanceOrbis(LightEntity):
         success = await self.hass.async_add_executor_job(turn_on)
         if success:
             self._is_on = True
+            if ATTR_BRIGHTNESS in kwargs:
+                self._brightness = kwargs[ATTR_BRIGHTNESS]
             self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
@@ -92,12 +108,4 @@ class LedvanceOrbis(LightEntity):
 
     async def async_update(self):
         """Fetch new state data for this light."""
-        def get_status():
-            try:
-                return self._device.status()
-            except Exception:
-                return None
-
-        status = await self.hass.async_add_executor_job(get_status)
-        if status is not None:
-            self._is_on = status.get('dps', {}).get('20', False)
+        def get
