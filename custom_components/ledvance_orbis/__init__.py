d4538adecaebbd1265ffa11cfc76e52c427369e0
oktaydegerli <oktaydegerli@gmail.com>
2024-11-03 17:25:33 +0300

    Update

--- custom_components/ledvance_orbis/__init__.py
+++ custom_components/ledvance_orbis/__init__.py
@@ -1,1 +1,21 @@
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
+DOMAIN = "ledvance_orbis"
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Bileşeni başlatır."""
+    _LOGGER.info("Ledvance Orbis bileşeni başlatıldı")
+    hass.data[DOMAIN] = {}
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Config flow ile eklenen bileşenleri ayarlar."""
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    _LOGGER.info("Ledvance Orbis cihazı eklendi: %s", entry.data)
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Config flow ile eklenen bileşen kaldır"""
